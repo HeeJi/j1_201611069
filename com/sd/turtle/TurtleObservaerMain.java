614ae18d6de9e516a90c498bfdb5bfe4510d0130
p2_201611069 <gmlwl8466@naver.com>
2017-06-15 15:11:32 +0900

    Add files via upload

---
+++ com/sd/turtle/TurtleObservaerMain.java
@@ -0,0 +1,24 @@
+package com.sd.turtle;
+import java.util.Observer;
+import java.util.Observable;
+import java.util.ArrayList;
+import java.awt.geom.Point2D.Double;
+import java.awt.Color;
+import ch.aplu.turtle.*;
+
+public class TurtleObserverMain {
+    public static void main(String[] args) {
+        System.out.println("--Observer pattern demo");
+        //turtle subject
+        BabyTurtle baby=new BabyTurtle();
+        //turtle observer
+        MomTurtle mom=new MomTurtle();
+        baby.addObserver(mom);
+        //mom goes to somewhere
+        mom.moveTo(100,100);
+        //baby moves and let mom know it, weather는 display, turtle은 main에서
+        baby.move(100,-100);
+        baby.setChanged();
+        baby.notifyObservers();
+    }
+}
